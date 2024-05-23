bf2e1f3ff8e17fc79fa662bd971baa27d878a01d
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2024-05-23 20:09:14 -0300

    Correccion de encriptar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 var bcrypt = require("bcrypt");
 
-var hassPass = function (pass) {
-  return bcrypt.hash(pass, 10);
+var hassPass = async function (pass) {
+  return await bcrypt.hash(pass, 10);
 };
 
-var descriptPass = function (pass, password_encriptado) {
-  return bcrypt.compare(pass, password_encriptado).then(function (comparePass) {
-    if (comparePass) {
-      return "Acceso permitido";
-    } else {
-      return "Acceso denegado";
-    }
-  });
+var descriptPass = async function (pass, password_encriptado) {
+  return await bcrypt
+    .compare(pass, password_encriptado)
+    .then(function (comparePass) {
+      if (comparePass) {
+        return "Acceso permitido";
+      } else {
+        return "Acceso denegado";
+      }
+    });
 };
 
 module.exports = { hassPass: hassPass, descriptPass: descriptPass };
