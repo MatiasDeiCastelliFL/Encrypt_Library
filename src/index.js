390450b32c61fe4080b9c2a0a5d803ca48e9c1fc
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2024-05-23 19:56:32 -0300

    Repair encryptar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import * as bcrypt from "bcrypt";
-const hassPass = async (pass) => {
-  return await bcrypt.hash(pass, 10);
+var bcrypt = require("bcrypt");
+
+var hassPass = function (pass) {
+  return bcrypt.hash(pass, 10);
 };
-const descriptPass = async (pass, password_encriptado) => {
-  const comparePass = await bcrypt.compare(pass, password_encriptado);
-  if (comparePass) {
-    return "Acceso permitido";
-  } else {
-    return "Acceso denegdo";
-  }
+
+var descriptPass = function (pass, password_encriptado) {
+  return bcrypt.compare(pass, password_encriptado).then(function (comparePass) {
+    if (comparePass) {
+      return "Acceso permitido";
+    } else {
+      return "Acceso denegado";
+    }
+  });
 };
-module.exports = { hassPass, descriptPass };
+
+module.exports = { hassPass: hassPass, descriptPass: descriptPass };
